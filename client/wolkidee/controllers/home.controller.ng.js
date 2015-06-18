bb94627f1624b92ce001e12671780edaf377b053
Erik Brandsma <erik.brandsma@outlook.com>
2015-06-18 12:15:56 +0200

    centreren home + foto i.p.v. foto of video

--- client/wolkidee/controllers/home.controller.ng.js
+++ client/wolkidee/controllers/home.controller.ng.js
@@ -13,6 +13,27 @@ angular.module('wolkidee.controllers', []).controller('HomeCtrl', function($scop
 			    	columnWidth: 0
 			    }
   			});
+
+  			$(document).ready(function(){
+  				$('.grid').css('margin', 'auto');
+			    $scope.setContainerWidth();
+			});
+
+			  $(window).resize(function(){
+			    $scope.setContainerWidth();
+			});
 		});
+
+		$scope.setContainerWidth = function(){
+			$('.grid').css('width', 'auto'); //reset
+			var windowWidth = $(document).width();
+			var blockWidth = $('.quoteCard').outerWidth(true);
+			console.log("Blockwidth: " + blockWidth)
+			var maxBoxPerRow = Math.floor(windowWidth / blockWidth);
+			var newWidth = Math.floor(maxBoxPerRow * blockWidth);
+			console.log(newWidth)
+			$('.grid').width(newWidth);
+			$('.grid').css('marg', 'auto'); //reset
+		}
 
 });
