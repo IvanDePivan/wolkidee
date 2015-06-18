564ab1c2325017188d79ab26776187248079d0dc
Ivan Horn <ivanhorn1990@gmail.com>
2015-06-18 11:41:31 +0200

    uploaden quote werkt (zonder image)
    + gitignore project file sublime

--- client/wolkidee/controllers/input.controller.ng.js
+++ client/wolkidee/controllers/input.controller.ng.js
@@ -1,10 +1,10 @@
-angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
+angular.module('wolkidee.controllers').controller('InputCtrl', function($scope, $meteor){
 
 	$scope.ticButtonClick = function(){
 		var name = $("#nameInput").val();
 		var title = $("#titleInput").val();
 		var quote = $("#quoteInput").val();
-		var good = true
+		var good = true;
 
 		//Check name validity
 		good = $scope.checkValidity({
@@ -15,7 +15,7 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 			maxCharacters: "20",
 			defaultPlaceholder: "Voor en achternaam!",
 			errorPlaceholder: "De voor en achternaam moet tussen 2 en 20 karakters bevatten!"
-		})
+		});
 
 		//Check title validity
 		good = $scope.checkValidity({
@@ -26,7 +26,7 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 			maxCharacters: "20",
 			defaultPlaceholder: 'Titel, bijvoorbeeld "Dag Avans!"',
 			errorPlaceholder: "De titel moet tussen 2 en 20 karakters bevatten!"
-		})
+		});
 
 		//Check quote validity
 		good = $scope.checkValidity({
@@ -37,10 +37,11 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 			maxCharacters: "400",
 			defaultPlaceholder: "Je wens, bericht, anekdote of groet.",
 			errorPlaceholder: "De wens, bericht, anekdote of groet moet tussen 2 en 400 karakters bevatten!"
-		})
-		if(good == true) {
+		});
+
+		if(good === true) {
 			var imageSource;
-			var images = document.getElementById("uploadBtn").files
+			var images = document.getElementById("uploadBtn").files;
 				if (images && images[0]) {
 			    var reader = new FileReader();
 			    reader.onload = function (e) {
@@ -50,12 +51,12 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 			    };
 			    reader.readAsDataURL(images[0]);
 			}
-			var quoteTemplate = '<div class="grid-item-swal thumbnail">'
-			quoteTemplate += '<img id="showChosenImage" src="http://placehold.it/260x130/EEE" style="height: 130px; overflow:auto;">'
-			quoteTemplate += '<div class="caption"><h4>' + name + '</h4><h4>' + title +'</h4>'
-			quoteTemplate += '<p>' + quote + '</p></div>'
-			quoteTemplate += '</div><br />'
-			quoteTemplate += '<p style="color: black;">Is dit goed?</p><br />'
+			var quoteTemplate = '<div class="grid-item-swal thumbnail">';
+			quoteTemplate += '<img id="showChosenImage" src="http://placehold.it/260x130/EEE" style="height: 130px; overflow:auto;">';
+			quoteTemplate += '<div class="caption"><h4>' + name + '</h4><h4>' + title +'</h4>';
+			quoteTemplate += '<p>' + quote + '</p></div>';
+			quoteTemplate += '</div><br />';
+			quoteTemplate += '<p style="color: black;">Is dit goed?</p><br />';
 			console.log("showSwall");
 			swal({   
 				title: "Je quote zal er zo uit zien:",   
@@ -68,13 +69,21 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 				closeOnConfirm: false,   closeOnCancel: false 
 			}, function(isConfirm){   
 					if (isConfirm) {     
-						swal("Gelukt!", "Jou quote is verstuurd!", "success");   
+						   
+						Quotes.insert({
+							'name': name,
+							'title': title,
+							'quote': quote,
+							'image': 'http://placehold.it/300'
+						}, function(){
+							swal("Gelukt!", "Jou quote is verstuurd!", "success");
+						});
 					} else {    
 						swal.close();
 					} 
 				});
 		}
-	}
+	};
 
 	$scope.checkValidity = function(arg){
 		if(arg.fieldToCheck === "" || arg.fieldToCheck.length < 2){
@@ -85,7 +94,7 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 			  $(arg.formGroupId).removeClass("has-error");
 			  $(arg.inputId).attr("placeholder", arg.defaultPlaceholder);
 			});
-			return false
+			return false;
 		} else if(arg.fieldToCheck.length > arg.maxCharacters){
 				sweetAlert({
 					title: "Oeps!", 
@@ -95,5 +104,5 @@ angular.module('wolkidee.controllers').controller('InputCtrl', function($scope){
 		}else {
 			return true;
 		}
-	}
+	};
 });
