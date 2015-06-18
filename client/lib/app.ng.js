9d8a1752f0b5bcd32a29f9233f09eadcee4360e8
Ivan Horn <ivanhorn1990@gmail.com>
2015-06-18 11:07:06 +0200

    collectie ophalen in home scherm, isotope werkend maken met ng-repeat

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,6 +1,6 @@
 Quotes = new Mongo.Collection("quotes");
 
-angular.module('wolkidee',['angular.snackbar', 'angular-meteor', 'ui.router', 'wolkidee.controllers']);
+angular.module('wolkidee',['angular.snackbar', 'angular-meteor', 'ui.router', 'wolkidee.controllers', 'iso', 'wolkidee.directives']);
 
 angular.module('wolkidee').run(function(){
 	// console.log('Booted');
--- client/wolkidee/controllers/home.controller.ng.js
+++ client/wolkidee/controllers/home.controller.ng.js
@@ -1,14 +1,18 @@
-angular.module('wolkidee.controllers', []).controller('HomeCtrl', function($scope){
+angular.module('wolkidee.controllers', []).controller('HomeCtrl', function($scope, $meteor){
 	$scope.title = 'test idee';
 
-  	    $('.grid').isotope({
-  	    	percentPosition: true,
-    		itemSelector: '.grid-item',
-		    masonry: {
-		    	columnWidth: 0
-		    }
-  		});
 
   		$scope.quotes = $meteor.collection(Quotes);
 		console.log($scope.quotes);
+
+  	    $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+		    $('.grid').isotope({
+	  	    	percentPosition: true,
+	    		itemSelector: '.grid-item',
+			    masonry: {
+			    	columnWidth: 0
+			    }
+  			});
+		});
+
 });
